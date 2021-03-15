690664da0970180f81ba0a7dbfcea34bcdf6b4f9
aramcho206 <aramcho206@gmail.com>
2021-03-15 11:11:39 +0900

    Add: comento homework first setting complete

---
+++ src/Pages/Main/Main.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom';
+class Main extends Component {
+    render() {
+      return (
+        <>
+          <div>hello world</div> 
+        </>
+      );
+    }
+  }
+  
+  export default withRouter(Main);
